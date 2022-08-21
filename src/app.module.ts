e230b7662cc5a5e48e056ed9f5913672e898db9a
Catalin <uzy_electro@yahoo.com>
2022-08-21 16:54:16 +0300

    create config orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,10 @@ import { getEnvPath } from "./common/helper/env.helper";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TaskModule } from "./task/task.module";
-import { Task } from "./task/task.entity";
 import { UserModule } from "./user/user.module";
-import { User } from "./user/user.entity";
 import { AuthModule } from "./auth/auth.module";
 import { PassportModule } from "@nestjs/passport";
+import { TypeOrmConfig } from "./typeorm.config";
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
@@ -24,17 +23,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 			envFilePath,
 			isGlobal: true
 		}),
-		TypeOrmModule.forRoot({
-			type: "postgres",
-			host: "127.0.0.1",
-			port: 5432,
-			username: "postgres",
-			password: "admin",
-			database: "to-do-db",
-			synchronize: true,
-			entities: [Task, User],
-			migrations: ["./src/migrations/*.ts"]
-		}),
+		TypeOrmModule.forRoot(TypeOrmConfig),
 	],
 	controllers: [AppController],
 	providers: [AppService],
